4344f983cb27da2d2a718e7b29a1ecee2d2969a6
Chukwuma Nnodim-Amadi <C.NnodimAmadi001@umb.edu>
2022-06-02 04:22:16 -0400

    Add files via upload
    ~ Added 2 new libraries(TcpStream & prelude)
    ~ Added necessary comments
    ~ Created a function to read through the Tcp stream
    ~ Created a buffer to store data in which the handle connection function will read through in order to establish a connection

--- main.rs
+++ main.rs
@@ -1,12 +1,25 @@
+// Listen for a TCP connection
 use std::net::TcpListener;
+// Read data through TCP stream
+use std::net:TcpStream;
+use std::io::prelude;
 
 fn main()  {
     let listener: TcpListener =
-        TcpListener::bind("127.0.0.1:8080").unwrap();
+    // The listener binds to the local address and listens for connections
+        TcpListener::bind("127.0.0.1:8080").unwrap(); 
 
     for stream: Result<TcpStream, Error> in listener.incoming() {
+    // Loop through our connections and determine which is established
         let stream: TcpStream = stream.unwrap();
 
-        println!("Connection Established");
+        // Call our handle_connection function to read the stream
+        handle_connection(stream);
     }
+}
+
+// Reads a TCP Stream
+fn handle_connection(mut stream: TCP Stream) {
+    // Buffer stores our data
+    let mut buffer: [i32; 1024] = [0; 1024];
 }
