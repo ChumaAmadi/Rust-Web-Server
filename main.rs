7c0ae38f9dc2fc0ebb66fc62a2b836e0127f5bc7
Chukwuma Nnodim-Amadi <C.NnodimAmadi001@umb.edu>
2022-06-04 21:40:00 -0400

    Add files via upload
    *Updates*
    ~ Added file system library
    ~ Fixed 20+ bugs, mainly operator errors & typos
    ~ Structured code to a more understandable format
    ~ Web server can now read through data in bytes and send a response once the expected response is validated

--- main.rs
+++ main.rs
@@ -1,15 +1,16 @@
 // Listen for a TCP connection
 use std::net::TcpListener;
 // Read data through TCP stream
-use std::net:TcpStream;
-use std::io::prelude;
+use std::net::TcpStream;
+use std::io::prelude::*;
+use std::fs;
 
 fn main()  {
     let listener: TcpListener =
     // The listener binds to the local address and listens for connections
         TcpListener::bind("127.0.0.1:8080").unwrap(); 
 
-    for stream: Result<TcpStream, Error> in listener.incoming() {
+    for stream in listener.incoming() {
     // Loop through our connections and determine which is established
         let stream: TcpStream = stream.unwrap();
 
@@ -19,7 +20,30 @@ fn main()  {
 }
 
 // Reads a TCP Stream
-fn handle_connection(mut stream: TCP Stream) {
-    // Buffer stores our data
-    let mut buffer: [i32; 1024] = [0; 1024];
+fn handle_connection(mut stream: TcpStream) {
+    // Buffer stores our request
+    let mut buffer = [0; 1024];
+    // Read through buffer
+    stream.read(&mut buffer).unwrap();
+    // Expected string response
+    let get = b"GET / HTTP/1.1\r\n";
+
+    // Checking if this is indeed the correct response
+    if buffer.starts_with(get) {
+        // Stores content of our html file into a readable string
+        let contents = fs::read_to_string("index.html").unwrap();
+        //  Calling the amount of bytes in the contents for string interpolation
+        let response = format!("HTTP/1.1 200 OK\r\nContent-Length: {}\r\n\r\n{}",
+        contents.len(), contents);
+
+        // Server receives response as bytes
+        stream.write(response.as_bytes()).unwrap();
+        // Server will read all the bytes before dumping them with flush and returning  a response each time
+        stream.flush().unwrap();
+
+    // Return an error 404 page if the response is incorrect
+    } else {
+    
+    }
+
 }
